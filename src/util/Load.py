44c23d9e1fea90c5daa2b5e789a4f725ed5a867e
v1ctor13 <victorviniciuspj@gmail.com>
2022-11-06 10:44:54 -0300

    load node added

--- src/util/Load.py
+++ src/util/Load.py
@@ -12,8 +12,9 @@ def execute(nodes):
         print(data)
         label = data[0]
         color = Colors.WHITE
-        x = data[1][0] # ERROR HERE
-        y = data[1][1]
+        pos = eval(data[1])
+        x = pos[0]
+        y = pos[1]
         newNode = Node(label, color, int(x), int(y))
 
         nodes.append(newNode)    
